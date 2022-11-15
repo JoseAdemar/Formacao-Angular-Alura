d7693f6b7b4d4e9c93c6fa7600e068c5e62224c4
= <=>
2022-11-14 23:34:01 -0300

    Estudando services do angular

---
+++ bytebank/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NovaTransferenciaCOmponent } from './nova-transferencia/nova-transferencia.componente';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ExtratoComponent } from './extrato/extrato.component';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { DEFAULT_ECDH_CURVE, DEFAULT_MAX_VERSION } from 'tls';
+
+registerLocaleData(localePt,'pt');
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NovaTransferenciaCOmponent,
+    ExtratoComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [
+     {provide: LOCALE_ID, useValue: 'pt' },
+     {provide: DEFAULT_CURRENCY_CODE,
+       useValue: 'BRL'
+     },
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
